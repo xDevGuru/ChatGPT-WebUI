289a907ff7bf1881f4c8c7e9482aa8ccd0a22d53
xDevGuru <100898720+xDevGuru@users.noreply.github.com>
2024-11-15 22:55:08 +0200

    Update home.tsx

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -95,7 +95,7 @@ export function useSwitchTheme() {
     );
 
     if (config.theme === "auto") {
-      metaDescriptionDark?.setAttribute("content", "#151515");
+      metaDescriptionDark?.setAttribute("content", "#000000");
       metaDescriptionLight?.setAttribute("content", "#fafafa");
     } else {
       const themeColor = getCSSVar("--theme-color");
